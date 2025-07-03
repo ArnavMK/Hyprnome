0ca3a85f4d553e238a30399e6aeaaea6210d45ee
ArnavMK <kothariarnav9@gmail.com>
2025-07-04 00:25:00 +0100

    Made Slider and Pause/Play Button Alternate text

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
 
 use std::ops::RangeInclusive;
-
 use eframe::egui;
 
-#[derive(Default)]
-struct Application {}
 
-impl eframe::App for Application {
+struct Hyprnome{
+    pub button_text: String
+}
+
+impl Default for Hyprnome {
+    fn default() -> Self {
+        Self {
+            button_text: String::from("Play")
+        }
+    }
+}
+
+impl eframe::App for Hyprnome{
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.vertical_centered(|ui| {
                 ui.add(egui::Slider::new(&mut 120, RangeInclusive::new(10, 500)));
+                ui.add_space(20.0);
+                let state_button = ui.button(&self.button_text);
+
+                if state_button.clicked() {
+                    match self.button_text.as_str() {
+                        "Play" => self.button_text = String::from("Pause"),
+                        "Pause" => self.button_text = String::from("Play"),
+                        _ => ()
+                    }
+                }
             });
         });
     }
@@ -18,5 +37,6 @@ impl eframe::App for Application {
 
 fn main() -> eframe::Result {
     let options = eframe::NativeOptions::default();
-    eframe::run_native("hyprnome", options, Box::new(|_cc| Ok(Box::<Application>::default())))
+    eframe::run_native("hyprnome", options, Box::new(|_cc| Ok(Box::<Hyprnome>::default())))
 }
+   
