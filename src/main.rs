3cf1da740d3cc4486c2c6c3f20e33e7a8883089a
ArnavMK <kothariarnav9@gmail.com>
2025-07-03 16:20:33 +0100

    Made metronome window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow, Button};
+const APP_ID: &str = "org.hypr_nome.188";
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder()
+        .application_id(APP_ID)
+        .build();
+
+    app.connect_activate(build_ui);
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    let button = Button::builder()
+        .label("Start")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    button.connect_clicked(|button| {
+        button.set_label("Started");
+    });
+
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .child(&button)
+        .build();
+
+    window.present();
 }
