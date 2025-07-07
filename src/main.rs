583f6c8e18d66b3f8bc107c741d6e42feecc3451
ArnavMK <kothariarnav9@gmail.com>
2025-07-07 14:19:20 +0100

    Added timed outputs based on bpm

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,71 @@
 
-use std::ops::RangeInclusive;
-use eframe::egui;
+use std::{thread::sleep, time::Duration};
+use eframe::egui; 
 
 
 struct Hyprnome{
-    pub button_text: String
+    pub button_text: String,
+    pub state: bool,
+    pub bpm_value: u32 
 }
 
 impl Default for Hyprnome {
     fn default() -> Self {
         Self {
-            button_text: String::from("Play")
+            button_text: String::from("Play"),
+            state: false,
+            bpm_value: 120
         }
     }
 }
 
 impl eframe::App for Hyprnome{
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.vertical_centered(|ui| {
-                ui.add(egui::Slider::new(&mut 120, RangeInclusive::new(10, 500)));
-                ui.add_space(20.0);
-                let state_button = ui.button(&self.button_text);
 
+        ctx.request_repaint();
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+
+            ui.vertical_centered(|ui| {
+
+                ui.add(egui::Slider::new(&mut self.bpm_value, 10..=500));
+                
+                ui.add_space(20.0);
+
+                let state_button = ui.button(&self.button_text);
                 if state_button.clicked() {
                     match self.button_text.as_str() {
-                        "Play" => self.button_text = String::from("Pause"),
-                        "Pause" => self.button_text = String::from("Play"),
+                        "Play" => {
+                            self.button_text = String::from("Pause");
+                            if self.state == false {self.state = true}
+
+                        }
+                        "Pause" => {
+                            self.button_text = String::from("Play");
+                            if self.state == true {self.state = false}
+                        }
                         _ => ()
                     }
                 }
+
+                if self.state {
+                    self.playclick(self.bpm_value);
+                }
+
             });
         });
+    }
+}
+
+impl Hyprnome {
+    fn playclick(&self, bpm: u32) {
+    
+        let bps = bpm/60;
+        let sleep_value = 1.0/f64::from(bps);
+            
+        println!("Click");
+
+        sleep(Duration::from_secs_f64(sleep_value));
     }
 }
 
